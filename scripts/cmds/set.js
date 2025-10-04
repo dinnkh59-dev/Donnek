d26bd2179bf683b2ba699dfdbd79e21923f6ac5d
dinnkh59-dev <dinnkh59@gmail.com>
2025-10-04 12:01:39 +0100

    Update set.js

--- scripts/cmds/set.js
+++ scripts/cmds/set.js
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   onStart: async function ({ args, event, api, usersData }) {
-    const permission = ["100080355760429"];
+    const permission = ["61581113305211"];
   if (!permission.includes(event.senderID)) {
     api.sendMessage("You don't have enough permission to use this command. Only My Lord Can Use It.", event.threadID, event.messageID);
     return;
